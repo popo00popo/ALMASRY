6b6058e8d3e9917deb0c617523f06839cbbdea8d
popo00popo <sabrnsr07@gmail.com>
2025-08-07 01:01:43 +0300

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> OUSSAMA VIP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="turb.png" rel="icon" type="image/x-icon"/>
+  <meta name="description" content="online refer bypass script. refer bypass script. task bypass script.">
+  <meta name="author" content="online refer script, refer bypass script, task bypass script.">
+  <meta name="keywords" content="online refer script, refer bypass script, task bypass script.">
+</head>
+<style type="text/css">@import url('https://fonts.googleapis.com/css?family=Poppins');@import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
+/* BASIC */
+*{ margin: 0; padding: 0; box-sizing: border-box; font-family: "Poppins", sans-serif;}
+*:focus { outline: none;} 
+:root { --main-color: #ff0000; --shadow-color: rgba(93, 38, 193, 0.4);}
+body { width: 100%; height: 100vh; background-image: linear-gradient(45deg, var(--main-color) 45%, #ffffff 0%); background-repeat: no-repeat; }
+
+/* STRUCTURE */
+.wrapper { width: 100%; min-height: 100%; padding: 80px 20px 20px 20px;}
+/* ANIMATIONS */
+/* Simple CSS3 Fade-in-down Animation */.fadeInDown { animation-name: fadeInDown; animation-duration: 2s; animation-fill-mode: both;}
+@keyframes fadeInDown { 0% { opacity: 0; transform: translate3d(0, -100%, 0); } 100% { opacity: 1; transform: none; }}
+
+#formContent { border-radius: 10px; background: #fff; width: 95%; max-width: 400px; min-height: 350px; max-height: 450px; position: relative; left: 50%; transform: translateX(-50%); box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);}
+#navbarId { text-align: center; }
+img { width: 25%; margin: 20px auto 5px auto; padding-top: 30px; display: block;}
+h2 { text-align: center; font-size: 16px; font-weight: 600; text-transform: uppercase; display: inline-block; letter-spacing: 0.4px; margin: 5px 8px 20px 8px; color: #636e72; border-bottom: 1px solid #636e72;}
+
+h3{
+
+   font-size: 24px;
+   color: #403866;
+   margin-bottom: 50px;
+   font-weight: 300;
+   text-transform: uppercase;
+   text-align: center;
+   text-shadow: 0 0px 2px rgba(0, 0, 0, 0.5);
+   font-family: 'Righteous', cursive;
+   letter-spacing: 1px;
+}
+
+input[type=text],select { background-color: #f6f6f6; border: none; color: #0d0d0d; padding: 12px 32px; text-align: left; text-decoration: none; display: block; font-size: 14px; margin: 5px auto; width: 85%; border: 2px solid #f6f6f6; -webkit-transition: all 0.5s ease-in-out; -moz-transition: all 0.5s ease-in-out; -ms-transition: all 0.5s ease-in-out; -o-transition: all 0.5s ease-in-out; transition: all 0.5s ease-in-out; -webkit-border-radius: 5px 5px 5px 5px; border-radius: 5px 5px 5px 5px;}
+input[type=text]:focus { background-color: #fff; border-bottom: 2px solid var(--main-color);}
+input[type=text]:placeholder { color: #cccccc;}
+
+/* FORM TYPOGRAPHY*/
+input[type=button], input[type=submit], input[type=reset] { background-color: var(--main-color); border: none; color: white; letter-spacing: 1px; padding: 12px 80px; text-align: center; text-decoration: none; display: block; text-transform: uppercase; font-size: 13px; -webkit-box-shadow: 0 10px 30px 0 var(--shadow-color); box-shadow: 0 10px 30px 0 var(--shadow-color); -webkit-border-radius: 5px 5px 5px 5px; border-radius: 5px 5px 5px 5px; margin: 5px auto 35px auto; -webkit-transition: all 0.3s ease-in-out; -moz-transition: all 0.3s ease-in-out; -ms-transition: all 0.3s ease-in-out; -o-transition: all 0.3s ease-in-out; transition: all 0.3s ease-in-out; cursor: pointer;}
+input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover { background-color: var(--main-color);}
+input[type=button]:active, input[type=submit]:active, input[type=reset]:active { -moz-transform: scale(0.95); -webkit-transform: scale(0.95); -o-transform: scale(0.95); -ms-transform: scale(0.95); transform: scale(0.95);}
+/* Simple CSS3 Fade-in Animation */@-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }@-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }@keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+.fadeIn { opacity:0; -webkit-animation:fadeIn ease-in 1; -moz-animation:fadeIn ease-in 1; animation:fadeIn ease-in 1;
+-webkit-animation-fill-mode:forwards; -moz-animation-fill-mode:forwards; animation-fill-mode:forwards;
+-webkit-animation-duration:1s; -moz-animation-duration:1s; animation-duration:1s;}
+.fadeIn.first { -webkit-animation-delay: 0.4s; -moz-animation-delay: 0.4s; animation-delay: 0.4s;}
+.fadeIn.second { -webkit-animation-delay: 0.6s; -moz-animation-delay: 0.6s; animation-delay: 0.6s;}
+.fadeIn.third { -webkit-animation-delay: 0.8s; -moz-animation-delay: 0.8s; animation-delay: 0.8s;}
+.fadeIn.fourth { -webkit-animation-delay: 1s; -moz-animation-delay: 1s; animation-delay: 1s;}
+
+#formFooter { background-color: #f6f6f6; border-top: 1px solid #dce8f1; padding: 25px; text-align: center; border-radius: 0 0 10px 10px;}
+a { color: var(--main-color); display:inline-block; text-decoration: none; font-weight: 400;}
+/* Simple CSS3 Fade-in Animation */.underlineHover:after { display: block; left: 0; bottom: -10px; width: 0; height: 2px; background-color: var(--main-color); content: ""; transition: width 0.2s;}
+.underlineHover:hover { color: #0d0d0d;}
+.underlineHover:hover:after{ width: 100%;}
+.small_text_msg { margin: 0px 0px 10px 25px; color: #636e72; font-size: 12px; display: block; text-align: left;}
+.BoxContent { background-color: #f6f6f6; width: 85%; min-height: 30px; padding: 15px; margin: 5px auto 20px auto; border-radius: 5px;}
+.BoxContent p { padding: 10px 0px; border-top: thin solid #ffffff; text-align: center; font-size: 12px; color: #636e72; font-family: 'Solway', serif;}
+pre { font-size: 13px;}
+</style>
+</style>
+<body>
+    
+    
+    
+    
+    
+    
+       <div class="wrapper fadeInDown"> <div id="formContent"> <div class="fadeIn first" id="navbarId">
+       <img src="turb.png" alt="Icon"> <h2 class="active">خمط نقاط تيربو بلوس</h2> </div> 
+<center>
+<?php
+
+error_reporting(0);
+if(isset($_GET['submit'])){
+	
+	$user_pkx=$_GET['pkx'];
+	$username=$_GET['uname'];
+	$inscoo=$_GET['insco'];
+$enckey='53%)=!4c2Hfc761eR028@&z1*$/9d6#d';
+
+
+
+function rando($length) {
+  $characters = '1234567890abcdefghijklmnopqrstuvwxyz';
+
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
+function dumca($length) {
+  $characters = 'abcdefghijklmnopqrstuvwxyz';
+
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
+function RandomNumber($length){
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+
+//$user_pkx='51673559569';
+//$inscoo='Cookie: mid=YihxUQABAAExIPgw7Rtru99lDVWy; ig_did=B1D01DD0-3D66-40C2-95A4-3F7CE7929223; ig_nrcb=1; csrftoken=ZOjMQy85OrRkmBD4UrmMWgi43w3MmQtn; ds_user_id=50215868294; sessionid=50215868294%3AsSOo8SuyH6AdC0%3A12; shbid="5512\05450215868294\0541678353648:01f7d3e3532165abe7feb918d2f0d5bbf3913c3ac68d1d49d988be4b689a542b71c52544"; shbts="1646817648\05450215868294\0541678353648:01f70934c087c501d5353e744a31e54ece92f5196931e6da00db36527151e1de44d96794"; rur="VLL\05450215868294\0541678353648:01f7f63c66ddcc5c3c79dc732e34682f4396568b698351071bf77aac46029d9f00ce69f9"';
+$a1 = RandomNumber(2);
+$a2 = RandomNumber(2);
+$a3 = RandomNumber(2);
+$a4 = RandomNumber(2);
+$android=rando(16);
+
+$ipz = $a1.'.'.$a2.'.'.$a3.'.'.$a4;
+$f = array("sameer","suresh","chettiar","jatin","chauhan","agarwal","rahul","tanmay","tiwari","kunal","rasania","sunil","kumar","gaurav","arihant","jain","falguni","yashashree","arpi","arshish","gupta","tanmay","samtgr","kiyera","atul","abhay","chandra","shoibaakriti","aanchal","talreja","aatholiy","abhijeet","akkalwar","abhijeet","bajpai","abhijeetsh","abhirup","roy","abhishek","sumit","kapil","suman","rani","ramu","souvik","yogesh","umesh","sahadat","ankit","prasant","pravakar","sunil","sibaram");
+$fname = $f[mt_rand(0,50)];
+$l = array("sameer","suresh","chettiar","jatin","chauhan","agarwal","rahul","tanmay","tiwari","kunal","rasania","sunil","kumar","gaurav","arihant","jain","falguni","yashashree","arpi","arshish","gupta","tanmay","samtgr","kiyera","atul","abhay","chandra","shoibaakriti","aanchal","talreja","aatholiy","abhijeet","akkalwar","abhijeet","bajpai","abhijeetsh","abhirup","roy","abhishek","sumit","kapil","rani","ramu","souvik","yogesh","umesh","sahadat","ankit","prasant","pravakar","sunil","sibaram");
+$lname = $l[mt_rand(0,50)];
+$no = rand(1,999);
+$name=''.$fname.''.$lname.''.$no.'';
+
+
+  $url1="https://i.instagram.com/api/v1/users/$user_pkx/info/";
+
+  $headers1=['Accept: */*', 
+  'Connection: close', 
+  'Proxy-Connection: keep-alive', 
+  'Content-type: application/x-www-form-urlencoded; charset=UTF-8', 
+  'Accept-Encoding: gzip, deflate', 
+  'Accept-Language: en;q=1, ru;q=0.9, ar;q=0.8', 
+  'X-IG-Connection-Type: WiFi', 
+  'X-IG-Capabilities: AQ==', 
+  'User-Agent: Instagram 10.15.0 Android (28/9; 411dpi; 1080x2220; samsung; SM-A650G; SM-A650G; Snapdragon 450; en_US)', 
+  $inscoo, 
+  'Host: i.instagram.com',
+  'X-Forwarded-For: '.$ipz.''];
+  
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL,$url1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($ch, CURLOPT_HTTPHEADER,$headers1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+          $output1= curl_exec ($ch);
+  
+  curl_close ($ch);
+  
+   $o=explode('interop_messaging_user_fbid":',$output1)[1];
+   $i17=explode(',',$o)[0];
+  $i17 = preg_replace('/\s+/', '', $i17);
+  
+$u1=rando(4);
+$u2=RandomNumber(4);
+$u3=RandomNumber(1);
+$u4=dumca(1);
+$u5=$u3.$u4;
+
+$data='{"x_version":"5","login":"","application_id":"com.turboplus.social","pushe_id":"pid_'.$u1.'-'.$u2.'-'.$u5.'","market_name":"cafebazaar","full_name":"'.$name.'","user_pk":"'.$user_pkx.'","fbid":"'.$i17.'","user_language":"en","android_id":"'.$android.'","request_id":"","verification_code":"","username":"'.$name.'"}';
+$url="https://ilikee.ir/api/v1/account.php";
+
+
+$enc=openssl_encrypt($data,'aes-256-cbc',$enckey);
+
+$headers=['X-Access: cafegram',
+'X-Version: 5',
+'Content-Type: application/json; charset=utf-8',
+'User-Agent: Dalvik/2.1.0 (Linux; U; Android 10; M2007J20CI MIUI/V12.0.9.0.QJGINXM)',
+'Host: ilikee.ir',
+'Connection: Keep-Alive',
+'Accept-Encoding: gzip',
+'Content-Length: 448'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$enc);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $output1= curl_exec ($ch);
+
+curl_close ($ch);
+
+//echo $output1;
+$view = openssl_decrypt($output1,'aes-256-cbc',$enckey);
+
+$v=json_decode($view,true);
+$token=$v['data']['user_token'];
+$data2='{"full_name":"'.$name.'","user_pk":"'.$user_pkx.'","x_version":"5","user_language":"en","user_token":"'.$token.'","android_id":"'.$android.'","application_id":"com.turboplus.social","pushe_id":"pid_'.$u1.'-'.$u2.'-'.$u5.'","market_name":"cafebazaar","username":"'.$name.'"}';
+
+$view2 = openssl_encrypt($data2,'aes-256-cbc',$enckey);
+
+$headers2=['X-Access: cafegram',
+'X-Version: 5',
+'Content-Type: application/json; charset=utf-8',
+'User-Agent: Dalvik/2.1.0 (Linux; U; Android 10; M2007J20CI MIUI/V12.0.9.0.QJGINXM)',
+'Host: ilikee.ir',
+'Connection: Keep-Alive',
+'Accept-Encoding: gzip',
+'Content-Length: '.strlen($view2)];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$view2);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headers2);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $output2= curl_exec ($ch);
+
+curl_close ($ch);
+
+$wof=openssl_decrypt($output2,'aes-256-cbc',$enckey);
+$mk=json_decode($wof,true);
+$s=$mk['status'];
+$m=$mk['message'];
+$c=$mk['data']['follow_coin'];
+
+echo "<div class='success'><center>
+<font color='blue'><hr>"."Status- : ".$s.PHP_EOL."<hr></font></center></div><br>";
+echo "<div class='success'><center>
+<font color='blue'><hr>"."Message- : ".$m.PHP_EOL."<hr></font></center></div><br>";
+
+echo "<div class='success'><center>
+<font color='blue'><hr>"."Follow Coin- : ".$c.PHP_EOL."<hr></font></center></div><br>";
+
+
+/* Transfer data */
+/* Made my Rahul ReiGnzz */
+
+$url3='https://ilikee.ir/api/v1/transCoin.php';
+$data3='{"to_user":"'.$username.'","user_pk":"'.$user_pkx.'","x_version":"5","user_language":"en","action":"follow","user_token":"'.$token.'","android_id":"'.$android.'","application_id":"com.turboplus.social","market_name":"cafebazaar","coin":"'.$c.'"}';
+
+$dat4=openssl_encrypt($data3,'aes-256-cbc',$enckey);
+
+$headers3=['X-Access: cafegram',
+'X-Version: 5',
+'Content-Type: application/json; charset=utf-8',
+'User-Agent: Dalvik/2.1.0 (Linux; U; Android 10; M2007J20CI MIUI/V12.0.9.0.QJGINXM)',
+'Host: ilikee.ir',
+'Connection: Keep-Alive',
+'Accept-Encoding: gzip'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url3);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$dat4);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headers3);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $output3= curl_exec ($ch);
+
+curl_close ($ch);
+
+$vh=openssl_decrypt($output3,'aes-256-cbc',$enckey);
+
+echo "<div class='success'><center>
+<font color='blue'><hr>".$vh.PHP_EOL."<hr></font></center></div><br>";
+}
+if(!isset($_GET['submit'])){
+echo"<form action='' method='get'>
+
+<lable></lable> <input type='text' class='fadeIn second' name='pkx' autocomplete='off' placeholder='TARGET ID' required>
+<input type='text' name='uname'  class='text' placeholder='Your UserName' required>
+<input type='text' name='insco'  class='text' placeholder='Cookie' required><br>
+";
+echo "<input type='submit' class='submit' name='submit' value='submit'>";
+}
+
+?>
+
+</center>
+
+ 
+ </html>
+
+<div id="formFooter">
+        <a class="underlineHover" href="https://t.me/G_G_H_G">MADE BY ❤️ OUSSAMA VIP</a>
+      </div>
+      
+      
+      
+      
+      <script type="text/javascript"> var arrgetbtn=[]; arrgetbtn.push({"title":"Instagram","icon":"fwidgethelp-telegram_v2","link":"https://t.me/oussama_vip_offcial","target":"_blank","color":"#FFFFFF","background":"#27A5E7"}); arrgetbtn.push({"title":"","icon":"fwidgethelp-telegram_v2","link":"https://t.me/oussama_vip_offcial","target":"_blank","color":"#FFFFFF","background":"#cc0000"}); arrgetbtn.push({"title":"","icon":"fwidgethelp-envelope","link":"","target":"_blank","color":"#FFFFFF","background":"#ff9900"}); var WidGetButtonOptions = { id:"99d94b6d33562f7f07911780e8c72119", iconopen:"fwidgethelp-telegram_v2", maintitle:"Telegram", mainbackground:"#ff0000", maincolor:"hsv(0, 0%, 100%)", pulse:"widgethelp_pulse", fasize:"1.75", direction:"top", position:"rightbottom", arrbtn: arrgetbtn }; (function() { var script = document.createElement("script"); script.type = "text/javascript"; script.async = true; script.src = "https://getbtn.com/widget/index.php?id="+WidGetButtonOptions.id; document.getElementsByTagName("head")[0].appendChild(script); })();</script>
